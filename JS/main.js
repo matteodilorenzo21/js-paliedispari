0bba1a370d8ea9e989678ffafca8b4f38779bdf6
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-06-06 17:32:10 +0200

    added 1st JS Script & fixed 1st Markdown

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,41 @@
 // JS LINK RUN CHECK
-console.log('JS RUNNING!')
+console.log('JS RUNNING!');
+
+// Prendiamo dal DOM gli elementi desiderati
+const submitBtn = document.getElementById('submit-btn');
+const displayResult = document.getElementById('result');
+
+// Definiamo la funzione checkPalindrome che verifica se una parola palindroma e in seguito restituisce un booleano
+function checkPalindrome(word) {
+
+// Convertiamo la parola in minuscolo e rimuoviamo gli spazi
+word = word.toLowerCase().replace(/\s/g, '');
+
+
+for (let i = 0; i < word.length / 2; i++) {
+
+    // Verifichiamo se il carattere corrente il carettere invertito sono diversi
+    if (word[i] !== word[word.length - 1 - i]) {
+
+    // Se i caratteri non corrispondono, restituiamo False perchè la parola non è palindroma
+    return false;
+    }
+}
+  // altrimenti restituiamo True
+  return true;
+}
+
+// Mettiamo in ascolto il bottone
+submitBtn.addEventListener('click', function() {
+
+// Otteniamo il valore dell'input dell'utente tramite l'ID 'word-input'
+const wordInput = document.getElementById('word-input').value;
+const word = checkPalindrome(wordInput);
+
+// Verifichiamo se la parola è un palindromo e aggiorniamo il testo nel displayResult di conseguenza
+if (word === true) {
+    displayResult.innerText = 'La parola è palindroma';
+    } else {   
+        displayResult.innerText = 'La parola non è palindroma';
+    }
+});
--- steps.md
+++ steps.md
@@ -1,9 +1,10 @@
-# CONTROLLO PALINDROMO
+# CONTROLLO DI UN PALINDROMO
 
-0. Ricavare gli elementi desiderati dal DOM;
-1. Chiedere all'utente di inserire una parola;
-2. Definire una funzione che compia i seguenti passaggi:
-    - Invertire l'ordine della parola inserita dall'utente;
-    - Confrontare il risultato invertito con la parola originale;
-        - **SE** il risultato è palindromo la funzione deve restituire True;
-        - **ALTRIMENTI** deve restituire False;
+1. Prendiamo dal DOM gli elementi desiderati tramite i loro ID.
+2. Definiamo la funzione checkPalindrome che compia le seguenti azioni:
+- Convertiamo la parola in minuscolo e rimuoviamo gli spazi. 
+- Confrontiamo i caratteri corrispondenti dalla prima metà della parola con i caratteri corrispondenti dalla seconda metà. 
+    - **Se** troviamo una discrepanza, la parola non è palindroma e restituiamo false.
+    - **Altrimenti**, la parola è palindroma e restituiamo true.
+3. Mettiamo in ascolto il click sul bottone submitBtn. Quando viene cliccato, otteniamo il valore dell'input dell'utente tramite l'ID word-input. Applichiamo la funzione checkPalindrome alla parola inserita e otteniamo il risultato.
+4. Aggiorniamo il testo nel displayResult a seconda se la parola è palindroma o meno.
