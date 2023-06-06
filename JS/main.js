a23c3be5198afc77038bae8e551effbfbbe2f275
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-06-06 17:54:11 +0200

    added 2nd JS Script & 2nd  Markdown

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
 // JS LINK RUN CHECK
 console.log('JS RUNNING!');
 
+// ? CONTROLLO DI UN PALINDROMO
+
 // Prendiamo dal DOM gli elementi desiderati
 const submitBtn = document.getElementById('submit-btn');
 const displayResult = document.getElementById('result');
@@ -38,4 +40,73 @@ if (word === true) {
     } else {   
         displayResult.innerText = 'La parola non è palindroma';
     }
+});
+
+
+
+
+
+
+// ? PARI E DISPARI
+
+// Generiamo un numero compreso tra 1 e 5
+function getRandomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+  }
+  
+  // Prendiamo gli elementi desiderati dal DOM;
+const playBtn = document.getElementById('play-btn');
+
+const cpuResult = document.getElementById('cpu-result');
+
+const sumResult = document.getElementById('sum-result');
+
+const playResult = document.getElementById('play-result');
+  
+// Mettiamo in ascolto il bottone
+playBtn.addEventListener('click', function() {
+
+    // Raccogliamo il valore scelto dall'utente
+    const userChoice = document.getElementById('user-choice').value;
+  
+    // Raccogliamo il numero inserito dall'utente convertito in un type number
+    const userNumber = parseInt(document.getElementById('user-number').value);
+  
+    // Verificare se il numero inserito dall'utente è maggiore di 5
+    if (userNumber > 5) {
+
+      // Se il numero è maggiore di 5, mostrare un messaggio di errore
+      const playResult = document.getElementById('play-result');
+      playResult.innerText = "Devi inserire un numero da 1 a 5!";
+
+      // altrimenti
+      return; 
+      
+    }
+  
+    // Generiamo un numero random per la CPU
+    const cpuNumber = getRandomNumber();
+  
+    // Calcolare la somma tra il numero scelto dall'utente e il numero dalla CPU
+    const sum = userNumber + cpuNumber;
+  
+    // Determinare se la somma è pari o dispari
+    const gameResult = sum % 2 === 0 ? 'pari' : 'dispari';
+  
+
+  
+    // Verifichiamo se il risultato corrisponde alla scelta dell'utente
+    if (gameResult === userChoice) {
+      // Se il risultato corrisponde, mostrare "Hai vinto!"
+      playResult.innerText = "Hai vinto!";
+    } else {
+      // Altrimenti, mostrare "Hai perso!"
+      playResult.innerText = "Hai perso!";
+    }
+  
+    // Mostrare il numero generato dalla CPU
+    cpuResult.innerText = "Cpu: " + cpuNumber;
+  
+    // Mostrare la somma dei numeri
+    sumResult.innerText = "Totale: " + sum;
 });
--- steps.md
+++ steps.md
@@ -1,10 +1,24 @@
 # CONTROLLO DI UN PALINDROMO
 
 1. Prendiamo dal DOM gli elementi desiderati tramite i loro ID.
-2. Definiamo la funzione checkPalindrome che compia le seguenti azioni:
+2. Dichiariamo la funzione checkPalindrome che compia le seguenti azioni:
 - Convertiamo la parola in minuscolo e rimuoviamo gli spazi. 
 - Confrontiamo i caratteri corrispondenti dalla prima metà della parola con i caratteri corrispondenti dalla seconda metà. 
     - **Se** troviamo una discrepanza, la parola non è palindroma e restituiamo false.
     - **Altrimenti**, la parola è palindroma e restituiamo true.
 3. Mettiamo in ascolto il click sul bottone submitBtn. Quando viene cliccato, otteniamo il valore dell'input dell'utente tramite l'ID word-input. Applichiamo la funzione checkPalindrome alla parola inserita e otteniamo il risultato.
 4. Aggiorniamo il testo nel displayResult a seconda se la parola è palindroma o meno.
+
+
+# PARI E DISPARI
+
+1. Dichiariamo la funzione getRandomNumber che genera un numero casuale compreso tra 1 e 5.
+2. Recuperiamo i vari elementi del DOM.
+3. Aggiungiamo un event listener al bottone che:
+    - Verifica se il numero inserito dall'utente è maggiore di 5. In tal caso, viene mostrato un messaggio di errore e la funzione viene interrotta.
+    - Genera un numero casuale per la CPU.
+    - Calcola la somma tra il numero scelto dall'utente e il numero generato dalla CPU.
+    - Determina se la somma è pari o dispari utilizzando l'operatore modulo.
+    - Confronta il risultato del gioco con la scelta dell'utente e mostra il risultato corrispondente.
+    - Mostra il numero generato dalla CPU.
+    - Mostra la somma dei numeri.
